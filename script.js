bfdc44db69ac403feecbcd2ca98acaea659968b5
Carjardying <carolinejardy.pro@gmail.com>
2025-03-03 12:33:36 +0100

    exercice3 partie 1 et 2 ok

---
+++ script.js
@@ -0,0 +1,19 @@
+let tmp = 15;
+
+// en if / else if / else
+
+if (tmp < 0) {
+  console.log("Freezing weather.");
+} else if (tmp < 10) {
+  console.log("Very cold weather.");
+} else if (tmp < 20) {
+  console.log("Cold weather.");
+} else if (tmp < 30) {
+  console.log("Normal in temp.");
+} else if (tmp < 40) {
+  console.log("Its Hot.");
+} else {
+  console.log("Its very hot.");
+}
+
+// ensuite en switchCase
