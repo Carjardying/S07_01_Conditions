a182f7f0b16e004b8e8ba8e90972149390c27040
Carjardying <carolinejardy.pro@gmail.com>
2025-03-03 14:19:44 +0100

    exercice3/3 done. Manque correction partie 1 p par Morgan

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let tmp = 15;
 
 // en if / else if / else
 
-if (tmp < 0) {
+/* if (tmp < 0) {
   console.log("Freezing weather.");
 } else if (tmp < 10) {
   console.log("Very cold weather.");
@@ -14,6 +14,27 @@ if (tmp < 0) {
   console.log("Its Hot.");
 } else {
   console.log("Its very hot.");
-}
+} */
 
 // ensuite en switchCase
+
+switch (true) {
+  case tmp < 0:
+    console.log("Freezing weather.");
+    break;
+  case tmp < 10:
+    console.log("Very cold weather.");
+    break;
+  case tmp < 20:
+    console.log("Cold weather.");
+    break;
+  case tmp < 30:
+    console.log("Normal in temp.");
+    break;
+  case tmp < 40:
+    console.log("It's hot.");
+    break;
+  case tmp < 50:
+    console.log("It's very hot.");
+    break;
+}
