d7ec1d070b85fcf769a715d7181c0d377f23f363
Carjardying <carolinejardy.pro@gmail.com>
2025-03-03 11:15:00 +0100

    exercice 1

---
+++ script.js
@@ -0,0 +1,22 @@
+let myNumber = 0;
+/* function isMyNumberPositive(num) {
+if (num > 0){
+    console.log(num + " est un nombre positif");
+} else if (num < 0 ){
+    console.log(num + " est un nombre négatif");
+} else{
+    console.log(num + " est un nombre neutre");
+}
+};
+isMyNumberPositive(myNumber); */
+
+
+// fonctionne aussi sans la fonction :
+
+if (myNumber > 0){
+    console.log(myNumber + " est un nombre positif");
+} else if (myNumber < 0 ){
+    console.log(myNumber + " est un nombre négatif");
+} else{
+    console.log(myNumber + " est un nombre neutre");
+}
